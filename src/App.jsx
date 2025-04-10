1a9c676fd6d44ccd6cc54fd87a56ea2e02939f2a
cho-sohyun <shteam1107@gmail.com>
2025-04-10 18:49:44 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import useCounterStore from "./stores/counterStore";
+
+function App() {
+  const { count, increase, increaseBy, decrease, decreaseBy } =
+    useCounterStore();
+  return (
+    <div className="App">
+      <h1>COUNT : {count}</h1>
+      <div className="increase">
+        <button onClick={increase}>1 증가</button>
+        <button onClick={() => increaseBy(10)}>10 증가</button>
+      </div>
+      <div className="decrease">
+        <button onClick={decrease}>1 감소</button>
+        <button onClick={() => decreaseBy(10)}>10 감소</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
